5da34e90f721e5a49bf823ff90f9bfb687c4c60e
Saroj Adhikari <saroj-adh123@hotmail.com>
2020-06-21 00:04:46 +0530

    Add project files.

---
+++ EnvVariableCredentials/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnvVariableCredentials
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //get values from user variables, the values need to be present in system.
+            var _usr = System.Environment.GetEnvironmentVariable("test_usr", EnvironmentVariableTarget.User);
+            var _pwd = System.Environment.GetEnvironmentVariable("test_pwd", EnvironmentVariableTarget.User);
+
+            Console.WriteLine("Username : " +_usr);
+            Console.WriteLine("Password : " +_pwd);
+
+        }
+    }
+}
